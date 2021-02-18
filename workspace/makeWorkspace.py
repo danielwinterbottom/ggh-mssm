274be5073e8b12c13eb0474fd3c9dfbeef7b28ef
Daniel Winterbottom <daniel.winterbottom@cern.ch>
2021-02-18 17:19:43 +0000

    adding new weights for powheg reference samples

--- workspace/makeWorkspace.py
+++ workspace/makeWorkspace.py
@@ -60,15 +60,12 @@ ROOT.gROOT.LoadMacro("RooSpline1D.cc+")
 
 w = ROOT.RooWorkspace('w')
 
+masses = [60, 80, 100, 120, 125, 130, 140, 160, 180, 200, 250, 300, 350, 400, 450, 500, 600, 700, 800, 900, 1000, 1200, 1400, 1600, 1800, 2000, 2300, 2600, 2900, 3200, 3500]
 
-masses_pythia_2018 = [80, 90, 100, 110, 120, 130, 140, 160, 180, 200, 250, 300, 400, 450, 600, 700, 800, 1200, 1400, 1500, 1600, 1800, 2000, 2600, 2900, 3200]
+masses_ref_2016 = [80, 90, 100, 110, 120, 130, 140, 160, 180, 200, 250, 300, 350, 400, 450, 500, 600, 700, 800, 900, 1000, 1200, 1400, 1500, 1600, 1800, 2000, 2300, 2600, 2900, 3200]
 
-masses_pythia_2016 = [80, 90, 100, 110, 120, 130, 140, 160, 180, 200, 250, 300, 350, 400, 450, 500, 600, 700, 800, 900, 1000, 1200, 1400, 1500, 1600, 1800, 2000, 2300, 2600, 2900, 3200]
-
-masses_pythia_2017 = [80, 90, 100, 110, 120, 130, 140, 180, 200, 250, 300, 350, 400, 450, 600, 700, 800, 900, 1200, 1400, 1500, 1600, 1800, 2000, 2300, 2600, 2900, 3200]
-
-if args.year == '2016': masses_pythia = masses_pythia_2016
-else: masses_pythia = list(set(masses_pythia_2018+masses_pythia_2017))
+if args.year == '2016': masses_ref = masses_ref_2016
+else: masses_ref = masses
 
 masses_ph = [60, 80, 100, 120, 125, 130, 140, 160, 180, 200, 250, 300, 350, 400, 450, 500, 600, 700, 800, 900, 1000, 1200, 1400, 1600, 1800, 2000, 2300, 2600, 2900, 3200, 3500]
 
@@ -134,25 +131,25 @@ for P in ['h', 'A', 'H']:
         wsptools.SafeWrapHist(w, ['h_pt'],
             GetNormed('input/mssm_pt_weight_inputs.root:%(P)s_m%(m)s_b%(u)s' % vars()), name='%(P)s_%(m)s_b%(u)s' % vars())
 
-for m in masses_pythia:
+for m in masses_ref:
 
     extra=''
     if args.year == '2016': extra = '_2016'
     wsptools.SafeWrapHist(w, ['h_pt'],
-        GetNormed('input/mssm_pt_weight_inputs.root:pythia_m%(m)s%(extra)s' % vars()), name='h_%(m)s_pythia' % vars())
+        GetNormed('input/mssm_pt_weight_inputs.root:ref_m%(m)s%(extra)s' % vars()), name='h_%(m)s_ref' % vars())
   
     if m not in masses_ph: continue
 
     for P in ['h', 'A', 'H']:
       for u in ['','_scale_up','_scale_down','_hdamp_up','_hdamp_down']:
 
-        # define binned weights here for mass points that exist for both pythia and powheg 
-        w.factory('expr::%(P)s_%(m)s_t_ratio%(u)s("@0/@1", %(P)s_%(m)s_t%(u)s, h_%(m)s_pythia)' % vars())
-        w.factory('expr::%(P)s_%(m)s_b_ratio%(u)s("@0/@1", %(P)s_%(m)s_b%(u)s, h_%(m)s_pythia)' % vars())
-        w.factory('expr::%(P)s_%(m)s_i_ratio%(u)s("@0/@1", %(P)s_%(m)s_i%(u)s, h_%(m)s_pythia)' % vars())
+        # define binned weights here for mass points that exist for both ref and powheg 
+        w.factory('expr::%(P)s_%(m)s_t_ratio%(u)s("@0/@1", %(P)s_%(m)s_t%(u)s, h_%(m)s_ref)' % vars())
+        w.factory('expr::%(P)s_%(m)s_b_ratio%(u)s("@0/@1", %(P)s_%(m)s_b%(u)s, h_%(m)s_ref)' % vars())
+        w.factory('expr::%(P)s_%(m)s_i_ratio%(u)s("@0/@1", %(P)s_%(m)s_i%(u)s, h_%(m)s_ref)' % vars())
 
 # now use linear interpolation between mass points
-masses_common = list(set(masses_pythia+masses_ph).intersection(masses_pythia).intersection(masses_ph))
+masses_common = list(set(masses_ref+masses_ph).intersection(masses_ref).intersection(masses_ph))
 masses_common.sort()
 min_mass = masses_common[0]
 max_mass = masses_common[-1]-1.
@@ -191,7 +188,7 @@ w.Print()
 if args.year == '2016':
   w.writeToFile('higgs_pt_2016_v0.root')
 else:
-  w.writeToFile('higgs_pt_v0.root')
+  w.writeToFile('higgs_pt_v1.root')
 
 w.Delete()
 
